739525723cb7764db7804e129e123ed9135bf539
Ernest Lu <eluindustries@gmail.com>
2024-03-03 16:33:50 -0800

    completed logic sim

--- logicsim/event.h
+++ logicsim/event.h
@@ -11,6 +11,10 @@ struct Event
 
 typedef struct EventLess {
         //write the operator() required to make this a functor that compares Events by time
+    bool operator()(const Event* event1, const Event* event2) const {
+        // min heap returns true if event1 is smaller than event2
+        return (event1->time) < (event2->time);
+    }
 } EventLess;
 	
 #endif
